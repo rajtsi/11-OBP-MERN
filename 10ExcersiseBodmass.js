4c19aedb2070724994d4dece1994c8bbec7d1ba3
Rajat Singh <rajatsingh.1634@gmail.com>
2026-08-27 15:11:27 +0530

    Arethematics and BodMass Practice/27/8/2026

---
+++ 10ExcersiseBodmass.js
@@ -0,0 +1,46 @@
+
+let a = 2.5;
+// You can write 0 to 10 lines of code here and no console.log can be used 
+// use at least * and -
+
+a = a * 2 - 2;
+console.log(a); // 3
+
+// You can write 0 to 10 lines of code here and no console.log can be used 
+// use at Most * and - and **
+
+console.log(a * 4) // 12 // a= 3
+
+// You can write 0 to 10 lines of code here and no console.log can be used 
+// use at least * and +
+a = a + 14;
+
+console.log(a % 10); // 7  // a=17
+
+a = a * 2 + 3 - 2 / 1;   // a=35 
+
+// You can write 0 to 10 lines of code here and no console.log can be used 
+// use at least - and +
+
+a = a - 30;
+
+console.log(a);//5  // a=5
+
+
+// You can write 0 to 10 lines of code here and no console.log can be used 
+// use at least **  
+
+a = a ** 4;
+
+console.log(a / 5); // 125 // a/5=125, a= 125*5= 625 =  5*4
+
+// You can write 0 to 10 lines of code here and no console.log can be used 
+a = "I completed my class work";
+
+console.log(a);  // I completed my class work
+
+
+
+
+
+
---
+++ 9Arethematic&BODMASS1.js.js
@@ -0,0 +1,29 @@
+//you can not use console.log in you code ;
+
+let a = 5;
+// You can write here some code one line or multiple line as you wish
+// div. and mul.
+a = a * 1.5;
+console.log(a);// it should print 7.5;
+
+// You can write here some code one line or multiple line as you wish
+// +,-,div.
+
+a = a - 3.75;
+console.log(a + 20) // 23.75     // a+20=23.75, a=23.75-20 =3.75
+// a = 3.75
+// You can write here some code one line or multiple line as you wish
+// only * and ** ,-,+
+a=a+5.25;
+a=(a**2)
+console.log(a) // 81 
+
+
+// 7.5
+// 23.75
+// 81
+
+
+
+
+
