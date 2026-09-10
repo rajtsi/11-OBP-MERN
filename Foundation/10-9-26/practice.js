001635ab655ebf537fdfb42894a440baa03472b1
Rajat Singh <rajatsingh.1634@gmail.com>
2026-09-10 15:01:31 +0530

    10-9-2026

---
+++ Foundation/10-9-26/practice.js
@@ -0,0 +1,35 @@
+// you have a number in variable x;
+// check if this number is divisible by 3 or Not ;
+
+//  sum of digits should be divisible by 3;
+
+
+// let x=2828820918181;
+
+// Yes
+// No
+
+// let count = 0;
+// let digitSum = 0;
+
+// let x = 200;
+
+// for (x; x > 0; x = Math.floor(x / 10)) {
+//     let lastDigit = x % 10
+//     digitSum += lastDigit;
+//     count++;
+// }
+
+// if(digitSum%3===0)
+// {
+//     //divisible
+// }
+// else
+// {
+//     //notDivisible
+// }
+
+
+
+
+
---
+++ Foundation/10-9-26/theory.js
@@ -0,0 +1,72 @@
+
+
+// for(let a=0;a<10;a++)
+// {
+
+//     break;-> // break will stop whole loop no anystep will be runned after its encountered and we wil come out of the loop;
+//     continue; //
+
+
+// }
+
+
+// let x = 1990000888; // 1+0+0+0+3;
+
+// // number of digit 
+// // sum of digits
+
+// let NumberOfdigit = 0;
+// let digtsSum = 0;
+
+// for (let y = x; y > 0; y = Math.floor(y / 10))  //10000 //1000, 1  41/10=> 4.1
+// {
+//     NumberOfdigit = NumberOfdigit + 1; //1//2//...../6
+//     digtsSum = digtsSum + (y % 10);
+// }
+// console.log(NumberOfdigit);
+// console.log(digtsSum);
+
+
+
+
+// break;
+// continue;
+
+//for-> for loop
+
+
+//while // whileloop
+
+// let a = 10;
+
+// while (a < 10) {
+//     console.log("a");
+//     // break;
+//     //if (a == 5) { break; }
+//     a++;
+// }
+
+
+// console.log(a);
+
+
+// let a = 1;
+// do {
+//     a++;
+//     if (a === 5) {
+//         continue;
+//     }
+    
+//     console.log(a);
+// } while (a < 10);
+
+
+let a=1;
+
+// print number from 1 to 15 only if its odd number 
+
+//1, 3, 5
+
+
+
+
