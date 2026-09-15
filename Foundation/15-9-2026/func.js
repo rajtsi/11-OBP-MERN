a4ccd7b5e1d2db4907dff3239dd569dd4fa7e755
Rajat Singh <rajatsingh.1634@gmail.com>
2026-09-15 15:01:47 +0530

    15-9-2026

---
+++ Foundation/15-9-2026/func.js
@@ -0,0 +1,19 @@
+//a,b,c -> and it will return greatest of these numbers
+
+function greatest(a, b, c) {
+
+
+}
+
+
+//you will get 3 parameters y,m,d
+
+// you have to write a function that will return true if this date is valid 
+// otherwise false 
+
+
+// let y=100;
+// let m=8;
+
+
+// make a fucntion that takes a prameter days and then convert it into year month adn remainkng day and print inside function
